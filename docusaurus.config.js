a063682ae50783be506ed6d24ce1079817d535ec
Nguyen Pham <nguyen.pham@katalon.com>
2022-03-03 09:49:33 +0700

    add docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,13 +25,13 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/will-nguyenpham/docusaurus-demo/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/will-nguyenpham/docusaurus-demo/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
