0dfaab300a7a8e2d3728d2a4d94f596456341791
Nguyen Pham <nguyen.pham@katalon.com>
2022-05-04 15:44:41 +0700

    update config url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Katalon docs',
   tagline: '',
-  url: 'https://nostalgic-babbage-c2a210.netlify.app/docs',
+  url: 'https://nostalgic-babbage-c2a210.netlify.app/',
   baseUrl: '/',
   onBrokenLinks: 'ignore',
   favicon: 'img/logo.svg',
