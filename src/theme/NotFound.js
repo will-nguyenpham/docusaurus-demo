a6dcdff218f7756b93275c189381533c96745ac2
Nguyen Pham <nguyen.pham@katalon.com>
2022-06-08 14:21:28 +0700

    update page not found

--- src/theme/NotFound.js
+++ src/theme/NotFound.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '@theme/Layout';
 import Translate, {translate} from '@docusaurus/Translate';
 import {PageMetadata} from '@docusaurus/theme-common';
 
 export default function NotFound() {
+
+  const [loaded, setLoaded] = useState(false);
+
   React.useEffect(() => {
     const origin = window.location.origin;
     const href = window.location.href;
     if (href.includes('/javadoc/')) {
       window.location.href = href.replace(origin + "/javadoc", 'https://api-docs.katalon.com');
+    } else {
+      setLoaded(true);
     }
   }, []);
   return (
-    <>
+    <>{loaded ? (<>
       <PageMetadata
         title={translate({
           id: 'theme.NotFound.title',
@@ -49,6 +54,7 @@ export default function NotFound() {
           </div>
         </main>
       </Layout>
+    </>) : (<></>)}
     </>
   );
 }
