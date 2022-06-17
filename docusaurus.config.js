7ea3d867d935edd8e53fa56bb3c751e7c60795b4
Nguyen Pham <nguyen.pham@katalon.com>
2022-06-17 10:50:11 +0700

    update redirect links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -276,6 +276,41 @@ const config = {
                 ]
             },
             {
+                to: "/docs/katalon-studio-enterprise/welcome-to-katalon-studio",
+                from: [
+                    "/docs/katalon-studio-enterprise/index.html",
+                    "/docs/katalon-studio-enterprise"
+                ]
+            },
+            {
+                to: "/docs/katalon-testops/overview/testops-overview",
+                from: [
+                    "/docs/katalon-testops/index.html",
+                    "/docs/katalon-testops"
+                ]
+            },
+            {
+                to: "/docs/katalon-runtime-engine/introduction-to-runtime-engine",
+                from: [
+                    "/docs/katalon-runtime-engine/index.html",
+                    "/docs/katalon-runtime-engine"
+                ]
+            },
+            {
+                to: "/docs/katalon-testcloud/overview",
+                from: [
+                    "/docs/katalon-testcloud/index.html",
+                    "/docs/katalon-testcloud"
+                ]
+            },
+            {
+                to: "/docs/katalon-recorder/get-started/overview",
+                from: [
+                    "/docs/katalon-recorder/index.html",
+                    "/docs/katalon-recorder"
+                ]
+            },
+            {
                 to: "/docs/katalium-framework/overview",
                 from: [
                     "/docs/katalium-framework/index.html",
