cf76c816bdd5e126fb642c1675413635d611a0f5
Nguyen Pham <nguyen.pham@katalon.com>
2022-06-30 10:58:35 +0700

    add tracking event next previous page

---
+++ src/theme/PaginatorNavLink/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+export default function PaginatorNavLink(props) {
+  const {permalink, title, subLabel, isNext} = props;
+  return (
+    <Link
+      className={clsx(
+        'pagination-nav__link',
+        isNext ? 'pagination-nav__link--next' : 'pagination-nav__link--prev',
+      )}
+      onClick={() => { 
+        if (subLabel.props.children === 'Next') {
+          analytics.track('Next Page');
+        } else if (subLabel.props.children === 'Previous') {
+          analytics.track('Previous Page');
+        }
+       }}
+      to={permalink}>
+      {subLabel && <div className="pagination-nav__sublabel">{subLabel}</div>}
+      <div className="pagination-nav__label">{title}</div>
+    </Link>
+  );
+}
